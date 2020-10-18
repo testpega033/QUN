1d01865d2c9ae977692aa53637121a9c231e6232
unknown <testpega033@gmail.com>
2020-10-18 14:36:26 +0530

    uploaded template spring boot project

---
+++ src/main/java/com/openforum/qun/QunApplication.java
@@ -0,0 +1,13 @@
+package com.openforum.qun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QunApplication.class, args);
+	}
+
+}
